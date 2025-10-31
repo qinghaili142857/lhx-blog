03fdfb52721d8a8d4533a253379afe3a5db1ae5b
qinghaili142857 <1411488925@qq.com>
2025-10-31 14:31:37 +0800

    test

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "yCENzh's Blog",
-	subtitle: "爱你所爱~",
+	title: "lhx's Blog",
+	subtitle: "愿你走过半生，归来仍是少年",
 	themeColor: {
 		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
